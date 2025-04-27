20b5f1887d25bbc01e009aa4bdb43af8c254bf7e
Immad Mir <mirimmad17@gmail.com>
2025-04-27 12:38:52 +0530

    feat: early exit on non linux systems

--- src/main.rs
+++ src/main.rs
@@ -9,37 +9,44 @@ use debugger::Debugger;
 use std::ffi::CString;
 
 fn main() -> anyhow::Result<()> {
-    // collect the args
-    let args = std::env::args().collect::<Vec<String>>();
-    if args.len() < 2 {
-        eprintln!("Usage: {} <program name>", args[0]);
+    #[cfg(not(target_os = "linux"))]
+    {
+        eprintln!("This platform is not supported");
         std::process::exit(-1);
     }
-    let program_name = CString::new(args[1].as_bytes())?;
-    let prog_args = CString::new("")?;
-
-    match unsafe { nix::unistd::fork() } {
-        Ok(nix::unistd::ForkResult::Parent { child }) => {
-            println!("Started debugger");
-            let mut debugger = Debugger::new(&args[1], child);
-            debugger.run()?;
-        }
-        Ok(nix::unistd::ForkResult::Child) => {
-            println!("Child process ID: {}", nix::unistd::getpid());
-            nix::sys::ptrace::traceme()?;
-            // set process personality to no address space randomization
-            unsafe { libc::personality(libc::ADDR_NO_RANDOMIZE as u64) };
-            nix::unistd::execve(
-                program_name.as_ref(),
-                &[program_name.as_ref()],
-                &[prog_args.as_ref()],
-            )?;
-        }
-        Err(e) => {
-            eprintln!("Error forking: {}", e);
+    #[cfg(target_os = "linux")]
+    {
+        // collect the args
+        let args = std::env::args().collect::<Vec<String>>();
+        if args.len() < 2 {
+            eprintln!("Usage: {} <program name>", args[0]);
             std::process::exit(-1);
         }
-    }
+        let program_name = CString::new(args[1].as_bytes())?;
+        let prog_args = CString::new("")?;
 
+        match unsafe { nix::unistd::fork() } {
+            Ok(nix::unistd::ForkResult::Parent { child }) => {
+                println!("Started debugger");
+                let mut debugger = Debugger::new(&args[1], child);
+                debugger.run()?;
+            }
+            Ok(nix::unistd::ForkResult::Child) => {
+                println!("Child process ID: {}", nix::unistd::getpid());
+                nix::sys::ptrace::traceme()?;
+                // set process personality to no address space randomization
+                unsafe { libc::personality(libc::ADDR_NO_RANDOMIZE as u64) };
+                nix::unistd::execve(
+                    program_name.as_ref(),
+                    &[program_name.as_ref()],
+                    &[prog_args.as_ref()],
+                )?;
+            }
+            Err(e) => {
+                eprintln!("Error forking: {}", e);
+                std::process::exit(-1);
+            }
+        }
+    }
     Ok(())
 }
